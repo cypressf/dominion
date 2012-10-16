0a41126dcff73d24c9a05a7ec38bbca9c2c89766
Cypress Frankenfeld <iamcypress@gmail.com>
2012-10-16 19:14:22 -0400

    heroku needs port info

--- app.py
+++ app.py
@@ -25,4 +25,6 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
