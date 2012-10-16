f8ccda4c06a51500a1f8fb971220188a782d0a22
Cypress Frankenfeld <iamcypress@gmail.com>
2012-10-16 18:51:51 -0400

    added card to database defn

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
 
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    expansion = db.Column(db.String(80), unique=False)
+
+    def __init__(self, name, expansion):
+        self.name = name
+        self.expansion = expansion
+
+    def __repr__(self):
+        return '<Card %r>' % self.name
