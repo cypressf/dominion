c04e138877e041a18cbe14bde6ce014fc57b1812
Cypress Frankenfeld <iamcypress@gmail.com>
2012-10-16 23:15:48 -0400

    wrote basic randomizer

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.expression import func, select
 import os
-from flask import render_template
+import random
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_BLUE_URL']
 db = SQLAlchemy(app)
 
+_EXPANSIONS = ["Dominion", "Intrigue", "Prosperity", "Seaside",  "Cornucopia", "Alchemy", "Hinterlands", "Dark Ages"]
 
 
 class Card(db.Model):
@@ -22,11 +25,42 @@ class Card(db.Model):
         return '<Card %r>' % self.name
 
 @app.route("/")
-def hello():
-    c = {"name": "Cypress"}
-    return render_template('base.html', bla=c)
+def home():
+    expansions = request.args.getlist("expansion")
+    if not expansions:
+        return render_template('home.html', all_expansions = _EXPANSIONS)
+    
+    # get a random number of cards for each expansion
+    ran = constrained_random(len(expansions), 10)
+    cards = {}
+    for i, e in enumerate(expansions):
+        if not ran[i]:
+            continue
+
+        cards[e] = []
+        card_query = Card.query.filter_by(expansion=e).order_by(func.random()).limit(ran[i])
+        
+        for card in card_query:
+            cards[e].append(card.name)
+
+    return render_template('home.html', cards = cards, expansions=expansions, all_expansions = _EXPANSIONS)
+
+def constrained_random(n, total):
+    """Return a randomly chosen list of n nonnegative integers summing to total.
+    Each such list is equally likely to occur."""
+
+    return [x - 1 for x in constrained_sum_sample_pos(n, total + n)]
+
+def constrained_sum_sample_pos(n, total):
+    """Return a randomly chosen list of n positive integers summing to total.
+    Each such list is equally likely to occur."""
+
+    dividers = sorted(random.sample(xrange(1, total), n - 1))
+    return [a - b for a, b in zip(dividers + [total], [0] + dividers)]
+
 
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
