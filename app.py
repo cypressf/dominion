5e4a17055b3fcd61fbd6a37c7dea56c5e347cbb0
Cypress Frankenfeld <iamcypress@gmail.com>
2012-10-17 03:09:13 -0400

    saved sets

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
+from urlparse import urlparse, urljoin
 from sqlalchemy.sql.expression import func, select
 import os
 import random
@@ -12,6 +13,11 @@ db = SQLAlchemy(app)
 _EXPANSIONS = ["Dominion", "Intrigue", "Prosperity", "Seaside",  "Cornucopia", "Alchemy", "Hinterlands"]
 
 
+collections = db.Table('collections',
+    db.Column('card_id', db.Integer, db.ForeignKey('card.id')),
+    db.Column('collection_id', db.Integer, db.ForeignKey('collection.id'))
+)
+
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
@@ -23,6 +29,11 @@ class Card(db.Model):
 
     def __repr__(self):
         return '<Card %r>' % self.name
+
+class Collection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cards = db.relationship('Card', secondary=collections, backref='collections')
+
 
 @app.route("/")
 def home():
@@ -53,6 +64,23 @@ def home():
 
     return render_template('home.html', cards = cards, expansions=expansions, all_expansions = _EXPANSIONS)
 
+@app.route("/save_collection", methods=['POST'])
+def save_collection():
+    make_collection_from_strings(request.form.getlist("cards"))
+    return redirect(url_for('home'))
+
+
+
+@app.route("/saved_sets")
+def collections():
+    collection_query = Collection.query.all()
+    collections = []
+    for c in collection_query:
+        collections.append(c.cards)
+
+    return render_template('collections.html', collections = collections)
+
+
 def constrained_random(n, total):
     """Return a randomly chosen list of n nonnegative integers summing to total.
     Each such list is equally likely to occur."""
@@ -66,6 +94,21 @@ def constrained_sum_sample_pos(n, total):
     dividers = sorted(random.sample(xrange(1, total), n - 1))
     return [a - b for a, b in zip(dividers + [total], [0] + dividers)]
 
+def make_collection_from_strings(card_strings):
+    cards = []
+    for c in card_strings:
+        print c
+        card = Card.query.filter_by(name=c).first()
+        if card:
+            cards.append(card)
+    make_collection(cards)
+
+def make_collection(cards):
+    c = Collection()
+    for card in cards:
+        if c != "Platinum" and c != "Colony":
+            c.cards.append(card)
+    db.session.commit()
 
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
